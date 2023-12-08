2bd98bad2a1d6429f5c8096136e44a4a703277e8
DongHwna Lee <meadding231@gmail.com>
2023-12-08 14:58:10 +0900

    feat: get Home, Shorts, Sub Icons
    세 가지 Icons 가져옴

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -16,6 +16,44 @@ export const HomeIcon = (props) => (
   </svg>
 );
 
+export const HomeOffIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    enable-background="new 0 0 24 24"
+    height="24"
+    viewBox="0 0 24 24"
+    width="24"
+    focusable="false"
+  >
+    <path d="m12 4.44 7 6.09V20h-4v-6H9v6H5v-9.47l7-6.09m0-1.32-8 6.96V21h6v-6h4v6h6V10.08l-8-6.96z"></path>
+  </svg>
+);
+
+export const ShortsOffIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24"
+    viewBox="0 0 24 24"
+    width="24"
+    focusable="false"
+  >
+    <path d="M10 14.65v-5.3L15 12l-5 2.65zm7.77-4.33-1.2-.5L18 9.06c1.84-.96 2.53-3.23 1.56-5.06s-3.24-2.53-5.07-1.56L6 6.94c-1.29.68-2.07 2.04-2 3.49.07 1.42.93 2.67 2.22 3.25.03.01 1.2.5 1.2.5L6 14.93c-1.83.97-2.53 3.24-1.56 5.07.97 1.83 3.24 2.53 5.07 1.56l8.5-4.5c1.29-.68 2.06-2.04 1.99-3.49-.07-1.42-.94-2.68-2.23-3.25zm-.23 5.86-8.5 4.5c-1.34.71-3.01.2-3.72-1.14-.71-1.34-.2-3.01 1.14-3.72l2.04-1.08v-1.21l-.69-.28-1.11-.46c-.99-.41-1.65-1.35-1.7-2.41-.05-1.06.52-2.06 1.46-2.56l8.5-4.5c1.34-.71 3.01-.2 3.72 1.14.71 1.34.2 3.01-1.14 3.72L15.5 9.26v1.21l1.8.74c.99.41 1.65 1.35 1.7 2.41.05 1.06-.52 2.06-1.46 2.56z"></path>
+  </svg>
+);
+
+export const SubOffIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    enable-background="new 0 0 24 24"
+    height="24"
+    viewBox="0 0 24 24"
+    width="24"
+    focusable="false"
+  >
+    <path d="M10 18v-6l5 3-5 3zm7-15H7v1h10V3zm3 3H4v1h16V6zm2 3H2v12h20V9zM3 10h18v10H3V10z"></path>
+  </svg>
+);
+
 export const TrendingIcon = (props) => (
   <svg
     {...props}
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,10 @@ import React from "react";
 import { useSelector } from "react-redux";
 import styled, { css } from "styled-components";
 
+import { HomeIcon, HomeOffIcon, ShortsOffIcon, SubIcon, SubOffIcon } from "./Icons";
+
+import { Button } from "antd";
+
 const Wrapper = styled.div`
   margin: 0;
   padding: 0;
@@ -14,7 +18,7 @@ const Wrapper = styled.div`
     `}
 
   .sidebar {
-    width: 200px;
+    width: 216px;
     background-color: #fff;
     color: #000;
     padding-left: 20px;
@@ -32,6 +36,12 @@ const Wrapper = styled.div`
   .sidebar a {
     color: #000;
     text-decoration: none;
+  }
+
+  .sidebar-item {
+    width: 100%;
+    display: flex;
+    justify-content: left;
   }
 
   .content {
@@ -53,13 +63,28 @@ function Sidebar() {
       <div className="sidebar">
         <ul>
           <li>
-            <a href="/">홈</a>
+            <a href="/">
+              <Button type="text" className="sidebar-item">
+                <HomeOffIcon />
+                <span style={{ marginLeft: "10px" }}>홈</span>
+              </Button>
+            </a>
           </li>
           <li>
-            <a href="/">Shorts</a>
+            <a href="/">
+              <Button type="text" className="sidebar-item">
+                <ShortsOffIcon />
+                <span style={{ marginLeft: "10px" }}>Shorts</span>
+              </Button>
+            </a>
           </li>
           <li>
-            <a href="/">구독</a>
+            <a href="/">
+              <Button type="text" className="sidebar-item">
+                <SubOffIcon />
+                <span style={{ marginLeft: "10px" }}>구독</span>
+              </Button>
+            </a>
           </li>
           <div class="horizontal-line"></div>
           <li>
@@ -72,10 +97,38 @@ function Sidebar() {
           <li>탐색</li>
           <br />
           <div class="horizontal-line"></div>
-          <li>설정</li>
-          <li>신고 기록</li>
-          <li>고객센터</li>
-          <li>의견 보내기</li>
+          <li>
+            <a href="/">
+              <Button type="text" className="sidebar-item">
+                <HomeIcon />
+                <span style={{ marginLeft: "10px" }}>설정</span>
+              </Button>
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <Button type="text" className="sidebar-item">
+                <HomeIcon />
+                <span style={{ marginLeft: "10px" }}>신고 기록</span>
+              </Button>
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <Button type="text" className="sidebar-item">
+                <HomeIcon />
+                <span style={{ marginLeft: "10px" }}>고객센터</span>
+              </Button>
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <Button type="text" className="sidebar-item">
+                <HomeIcon />
+                <span style={{ marginLeft: "10px" }}>의견 보내기</span>
+              </Button>
+            </a>
+          </li>
         </ul>
       </div>
     </Wrapper>
