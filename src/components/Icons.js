76a574f244227c7a3e15318e38cb4534489f5462
DongHwna Lee <meadding231@gmail.com>
2023-12-12 08:59:19 +0900

    feat: image upload icon 설정

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -130,6 +130,35 @@ export const FeedBackIcon = () => (
   </svg>
 );
 
+export const ImageUploadIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/1999/xlink"
+    viewBox="0 0 24 24"
+    preserveAspectRatio="xMidYMid meet"
+    focusable="false"
+    class="style-scope tp-yt-iron-icon"
+    width="24px"
+    height="24px"
+  >
+    <g
+      version="1.1"
+      x="0px"
+      y="0px"
+      width="24px"
+      height="24px"
+      viewBox="0 0 24 24"
+      enable-background="new 0 0 24 24"
+      class="style-scope tp-yt-iron-icon"
+    >
+      <path
+        d="M14,13.6l2.8,3.4h-5.4l0.3-0.4L14,13.6 M8.9,14.7l1.2,1.9l0.3,0.4H7.1L8.9,14.7 M14,12l-3,4l-2-3l-4,5h14L14,12L14,12z
+M21,7h-2v2h-2V7h-2V5h2V3h2v2h2V7z M13,4v6v1h1h6v9H4V4H13 M14,3H3v18h18V10h-7V3L14,3z"
+        class="style-scope tp-yt-iron-icon"
+      ></path>
+    </g>
+  </svg>
+);
+
 export const TrendingIcon = (props) => (
   <svg
     {...props}
