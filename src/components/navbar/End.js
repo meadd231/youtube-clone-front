2cafc2ba9f7647baa6e74dde24f74a4ed21b6dc0
DongHwna Lee <meadding231@gmail.com>
2024-01-09 17:31:49 +0900

    refactor: navbar 로그인에 따라 나오는 UI 변수로 변경

--- src/components/navbar/End.js
+++ src/components/navbar/End.js
@@ -10,46 +10,53 @@ import UserModal from "../UserModal";
 import UploadModal from "../UploadModal";
 
 function End() {
-  const { token, userData } = useSelector(state => state.user);
+  const { token, userData } = useSelector((state) => state.user);
   const [uploadModalOpen, setUploadModalOpen] = useState(false);
   const [userModalOpen, setUserModalOpen] = useState(false);
 
   const location = useLocation();
+  const loginedUi = (
+    <>
+      <Tooltip placement="bottom" title={"만들기"} arrow={false}>
+        <UploadIcon
+          className="upload-icon"
+          onClick={() => setUploadModalOpen(true)}
+        />
+      </Tooltip>
+      {uploadModalOpen && (
+        <UploadModal
+          closeUploadModal={() => setUploadModalOpen(false)}
+        ></UploadModal>
+      )}
+      <Avatar
+        src={`${process.env.REACT_APP_SERVER_URL}/uploads/avatars/${userData.avatar}`}
+        style={{ verticalAlign: "middle" }}
+        gap={0}
+        onClick={() => setUserModalOpen(true)}
+      >
+        {userData.nickname}
+      </Avatar>
+      {userModalOpen && (
+        <UserModal closeUserModal={() => setUserModalOpen(false)}></UserModal>
+      )}
+    </>
+  );
+  const notLoginedUi = (
+    <>
+      <Link to="/login" state={{ prev: location.pathname }}>
+        <div
+          className="login-btn"
+          style={{ color: "#065fd4", display: "flex" }}
+        >
+          <ProfileIcon />
+          <div>로그인</div>
+        </div>
+      </Link>
+    </>
+  );
   return (
     <div className="end">
-      {token ? (
-        <>
-          <Tooltip placement="bottom" title={"만들기"} arrow={false}>
-            <UploadIcon className="upload-icon" onClick={() => setUploadModalOpen(true)} />
-          </Tooltip>
-          {uploadModalOpen && (
-            <UploadModal closeUploadModal={() => setUploadModalOpen(false)}></UploadModal>
-          )}
-          <Avatar
-            src={`${process.env.REACT_APP_SERVER_URL}/uploads/avatars/${userData.avatar}`}
-            style={{ verticalAlign: "middle" }}
-            gap={0}
-            onClick={() => setUserModalOpen(true)}
-          >
-            {userData.nickname}
-          </Avatar>
-          {userModalOpen && (
-            <UserModal closeUserModal={() => setUserModalOpen(false)}></UserModal>
-          )}
-        </>
-      ) : (
-        <>
-          <Link to="/login" state={{ prev: location.pathname}}>
-            <div
-              className="login-btn"
-              style={{ color: "#065fd4", display: "flex" }}
-            >
-              <ProfileIcon />
-              <div>로그인</div>
-            </div>
-          </Link>
-        </>
-      )}
+      {token ? loginedUi : notLoginedUi}
     </div>
   );
 }
